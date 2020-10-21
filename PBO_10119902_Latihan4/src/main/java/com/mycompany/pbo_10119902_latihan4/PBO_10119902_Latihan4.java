b4d4421659b26fb288f87f01853d3393ca0bd66c
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:33:39 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan4/src/main/java/com/mycompany/pbo_10119902_latihan4/PBO_10119902_Latihan4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan4;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan4 {
+  
+    public void tambahKambing(){
+        
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah Ditambah: " + jumlahKambing);
+    }
+    public static void main(String[] args) {
+        PBO_10119902_Latihan4 kambingJantan = new PBO_10119902_Latihan4();
+        kambingJantan.tambahKambing();
+}
+}
+
+    
+
